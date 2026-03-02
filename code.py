f768a830c1beff8a273540dbb6cabea8b89da8f0
Asim Qurashi <asimtaseer68@gmail.com>
2026-03-02 13:41:22 -0800

    Working on code

--- code.py
+++ code.py
@@ -1,5 +1,9 @@
 import cv2
+import mediapipe as mp
 print("Cv is installed")
-import cvzone
+# import cvzone
 
 print('cvzone is installed')
+image=cv2.imread('Resources/Back Ground.png')
+cv2.imshow("image_",image)
+cv2.destroyAllWindows()
