1fa618cac0bc1fc4ded2a9ea151c636ba53df6a4
Asim Qurashi <asimtaseer68@gmail.com>
2026-03-08 11:22:30 -0700

    Deleted Extra files

--- code.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import mediapipe as mp
-print("Cv is installed")
-# import cvzone
-
-print('cvzone is installed')
-image=cv2.imread('Resources/Back Ground.png')
-cv2.imshow("image_",image)
-cv2.destroyAllWindows()
